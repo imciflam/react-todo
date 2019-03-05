94730027330923f3e38d09439e58c14ada1ac7d8
imciflam <33329377+imciflam@users.noreply.github.com>
2019-03-05 12:44:25 +0300

    works

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react' //rce shortcut
+import PropTypes from 'prop-types'
 
 export class TodoItem extends Component {
+    /*getStyle = () => 
+    {
+        if (this.props.todo.completed)
+        {
+            return
+            {
+                textDecoration:'line-through'
+            }
+        }
+        else
+        {
+            return
+            {
+                textDecoration:'none';
+            }
+        }
+    }*/
   render() {
     return (
-      <div>
+      <div style ={{backgroundColor: '#E8E8E8'}}>
         <p>{this.props.todo.title}</p>
       </div>
     )
   }
 }
 
+TodoItem.propTypes =
+{
+    todo:PropTypes.object.isRequired
+}
+
+const itemStyle = 
+{
+    backgroundColor: '#f4f4f4'
+}
 export default TodoItem
