99ded8dbdad04046498832d3bcf67bc94d8d744c
imciflam <33329377+imciflam@users.noreply.github.com>
2019-03-05 12:29:40 +0300

    proptypes

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';  
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types'
 
 class Todos extends Component {
   render() {
       console.log(this.props.todos);//props of todo 
     return this.props.todos.map((todo) => 
     (
-        <TodoItem key={todo.id} todo ={todo} />//unique key propsf
+        <TodoItem key={todo.id} todo ={todo} />//unique key props
     )
     );
   }
+}
+
+Todos.propTypes =
+{
+    todos:PropTypes.array.isRequired
 }
 
 export default Todos;
