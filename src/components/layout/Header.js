3f5bd379fb9381e9af64c8f0be39cee14498735b
imciflam <33329377+imciflam@users.noreply.github.com>
2019-03-11 12:45:53 +0300

    link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Header() {
   return (
     <header style={headerStyle}>
         <h1>To-Do List</h1>
+        <Link style ={linkStyle} to = "/"> Home</Link>|<Link style ={linkStyle} to = "/about">About</Link>
     </header>
   )
 }
@@ -12,4 +13,10 @@ const headerStyle={
     color:'#fff',
     textAlign:'center',
     padding:'10px'
+}
+
+const linkStyle = 
+{
+    color:'#fff',
+    textDecoration:'none'
 }
