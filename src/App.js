562a6764946de59f3e745dfad77760f1608e56e7
imciflam <33329377+imciflam@users.noreply.github.com>
2019-03-11 12:40:32 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import Header from './components/layout/Header'
 import uuid from 'uuid'
 
@@ -52,14 +54,21 @@ addTodo = (title) =>{
   render() {
     //console.log(this.state.todos);
     return (
+      <Router> 
       <div className="App">
       <div className = 'container'> 
       <Header/>
+      <Route exact path = '/' render = {props => (
+      <React.Fragment>
       <AddTodo addTodo = {this.addTodo}/>
-        <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo = {this.delTodo}/>   
-      </div>//slipping props into Todos 
+        <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo = {this.delTodo}/> 
+      </React.Fragment>  
+       )}/>
+       <Route path = "/about" component = {About}/>
       </div>
-    );
+      </div>
+      </Router>
+    );//slipping props into Todos 
   }
 }
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Todolist, react version</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
