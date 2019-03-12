51772db30594317fc27ced55bb3c2a0b7ec1fcb8
imciflam <33329377+imciflam@users.noreply.github.com>
2019-03-12 13:57:59 +0300

    Updates

---
+++ precache-manifest.8bd82e6881563e6d3e5c3971212fd7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "165bfd5cab31fffbdaf5",
+    "url": "/react-todo/static/js/main.165bfd5c.chunk.js"
+  },
+  {
+    "revision": "3ca407afa46c46057c95",
+    "url": "/react-todo/static/js/2.3ca407af.chunk.js"
+  },
+  {
+    "revision": "165bfd5cab31fffbdaf5",
+    "url": "/react-todo/static/css/main.e61274e1.chunk.css"
+  },
+  {
+    "revision": "1537bb3675c0f5134a201f3d8b2fb6ef",
+    "url": "/react-todo/index.html"
+  }
+];
